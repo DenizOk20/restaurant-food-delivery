b82777c6dc9fef496f445a94105f93b8c30bd086
Deniz Ök <deniz.ok2019@gmail.com>
2023-09-14 13:46:00 +0300

    added category page and product list

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -1,8 +1,30 @@
+import { pizzas } from '@/data'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const categoryPage = () => {
   return (
-    <div>categoryPage</div>
+    <div className='text-red-500 flex flex-wrap w-full'>
+      {pizzas.map(item => (
+        <Link 
+          key={item.id} 
+          href={`/category/${item.id}`}
+          className='w-full h-[60vh] sm:w-1/2 lg:w-1/3 border-b-2 border-r-2 border-red-500 even:bg-fuchsia-50 p-4 flex flex-col justify-between group'
+          >
+            { item.img &&
+            <div className='relative h-[80%]'>
+                <Image src={item.img} alt='categoryImage' fill className='object-contain'/>
+            </div>
+            }
+            <div className='flex flex-row justify-between items-center p-2'>
+              <h1 className='font-bold text-xl p-2'>{item.title}</h1>
+              <p className='group-hover:hidden text-xl'>{item.price}</p>
+              <button className='hidden group-hover:block bg-red-500 text-white p-2 rounded-md'>Add to cart</button>
+            </div>
+        </Link>
+      ))}
+    </div>
   )
 }
 
--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,8 +1,27 @@
+import { menu } from '@/data'
+import Link from 'next/link'
 import React from 'react'
 
 const menuPage = () => {
   return (
-    <div>menuPage</div>
+    <div className='h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] p-4 lg:px-20 xl:px-40 flex flex-col md:flex-row items-center'>
+      {
+        menu.map(category => (
+          <Link 
+            key={category.id}
+            href={`/menu/${category.slug}`}
+            style={{backgroundImage: `url(${category.img})`}}
+            className='h-1/3 w-full bg-cover px-3 md:h-1/2 md:p-3'
+            >
+              <div className={`text-${category.color} w-1/2`}>
+                <h1 className='text-2xl uppercase font-bold'>{category.title}</h1>
+                <p className='text-sm m-0 md:my-3'>{category.desc}</p>
+                <button className={`hidden 2xl:block p-2 rounded-md bg-${category.color} text-${category.color === "black" ? "white" : "red-500" } hover:opacity-90`}>Explore</button>
+              </div>
+          </Link>
+        ))
+      }
+    </div>
   )
 }
 
